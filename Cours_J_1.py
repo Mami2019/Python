4b64e643607afddaca6684a27279e7fcc39ba160
Rakotondranosy MAMIHAJA <mamihaja.rakotondranosy@gmail.com>
2025-09-19 23:16:11 +0200

    feat: Cours jour 1 190920252315

---
+++ Cours_J_1.py
@@ -0,0 +1,10 @@
+nom = "Mamihaja"
+age = 30
+taille= 1.72
+Actif = True
+
+print("Bonjour", nom)  
+
+reponse = input("Ton prenom ? ")
+age = int(input("Ton âge? "))
+print(f"Tu auras {age + 1} ans l'an prochain.")
