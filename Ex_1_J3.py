8472b3f8fb2a9c29b9298e40d0bf244ac35d7a3b
Rakotondranosy MAMIHAJA <mamihaja.rakotondranosy@gmail.com>
2025-10-06 23:47:22 +0200

    chore: python 061020252347

---
+++ Ex_1_J3.py
@@ -0,0 +1,6 @@
+"""
+Énoncé. Demande un entier N puis affiche tous les nombres de 1 à N inclus.
+"""
+N = int(input("N : "))
+for i in range (1, N+1):
+    print(i)
---
+++ Ex_2_J3.py
@@ -0,0 +1,7 @@
+N = int(input("N : "))
+Somme = 0
+i = 0
+while i <= N :
+    Somme = Somme + i
+    i += 1
+print(Somme)
