5916971e1d2fafcb13b85986fdd8b2fc69fc00fe
Rakotondranosy MAMIHAJA <mamihaja.rakotondranosy@gmail.com>
2025-12-17 23:13:48 +0100

    chore: Exercice 4 17122025_2

---
+++ Cours_J_4.py
@@ -0,0 +1,10 @@
+"""
+Chaînes de caractères
+Objectifs du jour
+Manipuler des chaînes (str)
+Indexation et tranches ([], [:])
+Méthodes utiles (lower, upper, strip, replace, find, len)
+Concaténation et f-strings
+"""
+texte = "  Python   "
+print(texte.strip())
